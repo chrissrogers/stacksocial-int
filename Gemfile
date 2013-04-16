a7d4bc9a0467e52a996eaa20e6ac1e8641f0663d
Christopher Rogers <chrissrogers@gmail.com>
2013-04-15 23:02:02 -0500

    [users] [auth] adds user model and twitter auth scheme
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- Gemfile
+++ Gemfile
@@ -4,8 +4,13 @@ gem 'rails', '3.2.12'
 
 gem 'pg'
 
+gem 'omniauth'
+gem 'omniauth-twitter'
+
 group :assets do
   gem 'sass-rails', '~> 3.2.3'
+  gem 'bourbon'
+  gem 'neat'
   gem 'uglifier',   '>= 1.0.3'
 end
 
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module StacksocialInt
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_mailer.default_url_options = { host: 'stacksocial-int.herokuapp.com' }
   end
 end
