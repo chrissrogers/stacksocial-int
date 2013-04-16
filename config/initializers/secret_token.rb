c2ef8bc84c30311676a0ce151a526eb56af9215a
Christopher Rogers <chrissrogers@gmail.com>
2013-04-15 21:00:01 -0500

    [hi] initial commit
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StacksocialInt::Application.config.secret_token = 'aa65f545f8faa8656ee284034788f43618f0d45f1ec91adca3039a44e72e123b73f8fdfd687a1fe1e6dfbe02ff00ce2c9c3e934e77a7778e3b9dacf4ba6124b8'
